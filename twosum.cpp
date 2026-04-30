4ac9d6601009bcb2dd1074689248aa3f4b75d70c
Raushan Raj <raushanraj6206@gmail.com>
2026-04-30 23:39:22 +0530

    Two sum Leetcode 01

---
+++ twosum.cpp
@@ -0,0 +1,40 @@
+//using hashing
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> ans;
+
+        for(int i = 0; i < nums.size(); i++){
+            int first = nums[i];
+            int sec = target - first;
+
+            if(m.find(sec) != m.end()){
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
+            }
+            m[first] = i;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+    
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = obj.twoSum(nums, target);
+
+    cout << "Indices: ";
+    for(int i : result) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
